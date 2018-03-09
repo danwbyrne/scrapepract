beb91c7f2cc4f56d9f5b44af88c4c0917d96ad41
Daniel Byrne <danwbyrne@gmail.com>
2018-03-08 21:17:02 -0500

    alright we are breaking this down
    big parsing time

--- src/main.py
+++ src/main.py
@@ -2,7 +2,15 @@ import urllib2
 from bs4 import BeautifulSoup
 
 def scrape(url):
-	pass
+	page = urllib2.urlopen(url)
+	parsed = BeautifulSoup(page, 'html.parser')
+
+	paragraphs = parsed.find_all('p')
+
+	for i in range(len(paragraphs)): 
+		line = str(paragraphs[i]).strip('\n \<p>\</p>')
+		print('%s    %s\n' % (i,line))
+
 
 
 
@@ -14,11 +22,7 @@ def scrape(url):
 
 def test():
 	scrape_page = 'http://www.carrollcountytimes.com/carrollliving/ph-cc-living-religion-20170622-story.html'
-	page = urllib2.urlopen(scrape_page)
-
-	parsed = BeautifulSoup(page, 'html.parser')
-
-	print(parsed)
+	scrape(scrape_page)
 
 if __name__ == "__main__":
 	test()
