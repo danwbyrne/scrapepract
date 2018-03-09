8cc47ae06bcccd304328f4268abe6d3d9856e76b
Daniel Byrne <danwbyrne@gmail.com>
2018-03-08 22:19:39 -0500

    parsing harder now
    almost done getting all the info scraped

--- src/main.py
+++ src/main.py
@@ -1,25 +1,69 @@
 import urllib2
 from bs4 import BeautifulSoup
+import re
 
+def parse_details(details):
+	pass
+
+
+
+def get_phone(info):
+	if info == None: return None
+	pattern = re.compile(".*?(\(?\d{3}\D{0,3}\d{3}\D{0,3}\d{4}).*?")
+	match   = re.match(pattern, info)
+
+	try:
+		return(match.group(0).strip(' '))
+
+	except:
+		return None
+
+def get_contact(info):
+	pass
+
+def get_link(info):
+	pass
+	
+
+
+#we are converting from cleaned paragraph strings to lists here
+def parse_paragraph(paragraph):
+	org, org_link, contact, phone, address = None, None, None, None, None
+
+	para_split       = paragraph.split('Information:')
+
+	#real lazy right here
+	try:
+		details, info = para_split[0], para_split[1]
+	except:
+		details, info = para_split[0], None
+
+	org, address = parse_details(details)
+
+	org_link = get_link(info)
+	contact = get_contact(info)
+	phone = get_phone(info)
+
+
+
+
+
+
+
+	return [address, phone]
+
+
+	#return org, org_link, contact, phone, address
 
 #for cleaning this awful looking paragraphs
-def clean_paragraph(paragraph):
+def convert_paragraph(paragraph):
 	line = str(paragraph).strip('\n \<p>\</p>')
 	line = line.strip('.')
 
 	if line.find('Return to top') != -1:
 		return False
 
-	information_ind = line.find('Information:')
-
-
-
-
-
-
-
-	return line
-
+	return parse_paragraph(line)
 
 def scrape(url):
 	page = urllib2.urlopen(url)
@@ -28,19 +72,11 @@ def scrape(url):
 	paragraphs = soup.find_all('p')
 
 	for i in range(len(paragraphs)): 
-		line    = clean_paragraph(paragraphs[i])
+		line    = convert_paragraph(paragraphs[i])
 
-		if i not in range(253,266) and i not in [0]:
+		if i not in range(253,266) and i != 0:
 			if line != False:
-				print('%s    %s\n' % (i,line))
-
-
-
-
-
-
-
-
+				print('%s:::%s\n' % (i, line))
 
 
 def test():
